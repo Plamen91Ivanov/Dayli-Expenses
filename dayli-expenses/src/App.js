e5338d682adfc82c36149948cf7147ff0df9414c
plamen <plamen91ivanov@abv.bg>
2021-05-07 09:37:48 +0300

    code refactoring

--- dayli-expenses/src/App.js
+++ dayli-expenses/src/App.js
@@ -6,21 +6,21 @@ import Expenses from './components/Expenses/Expenses';
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
+    title: 'Food',
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'Garden Tools', amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
-    title: 'Car Insurance',
+    title: 'Car',
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
     id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
+    title: 'New Desk',
+    amount: 150,
     date: new Date(2021, 5, 12),
   },
 ];
@@ -32,15 +32,8 @@ const App = () => {
     setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
-  };
-
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
-
+  }; 
+  
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
